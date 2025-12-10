911a1589da9dd41151e2a3982d0d656ea44aae7b
Suraj Jadhav <surajjadhav6272@gmail.com>
2025-12-11 01:18:33 +0530

    Implemented Product Catalog Service with validations, business rules, and REST API endpoints

--- src/main/java/com/phasezero/catalog_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/phasezero/catalog_service/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.phasezero.catalog_service.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,5 +21,32 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleDuplicate(DuplicatePartNumberException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(ex.getMessage());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAll(Exception ex) {
+
+        if (ex instanceof org.springframework.web.bind.MissingServletRequestParameterException) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Required request parameter is missing");
+        }
+
+
+        if (ex instanceof org.springframework.web.servlet.NoHandlerFoundException) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Invalid endpoint or URL not found");
+        }
+        if (ex instanceof org.springframework.web.bind.MethodArgumentNotValidException) {
+            String msg = ((org.springframework.web.bind.MethodArgumentNotValidException) ex)
+                    .getBindingResult()
+                    .getFieldErrors()
+                    .stream()
+                    .map(e -> e.getField() + ": " + e.getDefaultMessage())
+                    .findFirst()
+                    .orElse("Validation failed");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Something went wrong: " + ex.getMessage());
     }
 }
